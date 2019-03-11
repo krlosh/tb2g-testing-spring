ded7169eec5162e0114444ee716da3b947dc2d50
Carlos Henche <carlos.henche@digibis.com>
2019-03-11 15:54:01 +0100

    Use profiles to customize which beans are available

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterActiveProfileTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterActiveProfileTest.java
@@ -2,28 +2,24 @@ package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
-import org.springframework.samples.petclinic.sfg.LaurelWordProducer;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+@ActiveProfiles("yanny")
+@SpringJUnitConfig(classes = HearingInterpreterActiveProfileTest.TestConfig.class )
+class HearingInterpreterActiveProfileTest {
 
-@ActiveProfiles("inner-class")
-@SpringJUnitConfig(classes = HearingInterpreterInnerClassTest.TestConfig.class )
-class HearingInterpreterInnerClassTest {
-
-    @Profile("inner-class")
+    @Profile("yanny")
     @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
     static class TestConfig {
-        @Bean
-        HearingInterpreter hearingInterpreter(){
-            return new HearingInterpreter(new LaurelWordProducer());
-        }
     }
+
     @Autowired
     HearingInterpreter hearingInterpreter;
 
@@ -31,6 +27,6 @@ class HearingInterpreterInnerClassTest {
     void whatIHeard() {
         String word = this.hearingInterpreter.whatIHeard();
 
-        assertEquals("Laurel", word);
+        assertEquals("Yanny", word);
     }
 }
